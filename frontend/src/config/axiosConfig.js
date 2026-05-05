2d05ab15e73521a6b3c8c34c3cc29d1d32c53788
ANUGRAH555 <anugrahit20@gmail.com>
2026-05-05 10:19:48 +0530

    production deployment fixes

--- frontend/src/config/axiosConfig.js
+++ frontend/src/config/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 axios.interceptors.request.use(
   async (config) => {
-    const token = localStorage.getItem('accessToken');
+    const token = localStorage.getItem('token');
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
